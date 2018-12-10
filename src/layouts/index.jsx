e189d5f8efcfe8434cf6bfb0b5db227afe9637c9
xLsDg <xlsdg@qq.com>
2018-12-10 09:31:23 +0800

    feat(src): add layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Layout } from 'antd';
 
 import styles from './index.less';
 
-function BasicLayout(props) {
+function renderHeader(props) {
+  return <Layout.Header className={styles.header}>Header</Layout.Header>;
+}
+
+function renderContent(props) {
   const { children } = props;
 
-  return <div className={styles.main}>{children}</div>;
+  return <Layout.Content className={styles.content}>{children}</Layout.Content>;
+}
+
+function renderFooter(props) {
+  return <Layout.Footer className={styles.footer}>Footer</Layout.Footer>;
+}
+
+function BasicLayout(props) {
+  return (
+    <Layout className={styles.main}>
+      {renderHeader(props)}
+      {renderContent(props)}
+      {renderFooter(props)}
+    </Layout>
+  );
 }
 
 BasicLayout.propTypes = {
