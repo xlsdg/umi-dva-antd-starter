e8e206dae7efebd6641955bb367974cee8a45494
xLsDg <xlsdg@qq.com>
2019-01-12 21:18:50 +0800

    chore(mod): upgrade modules

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { Row, Col } from 'antd';
+import { Col, Row } from 'antd';
 import ReactLoading from 'react-loading';
 
 import styles from './index.less';
