53c65f33a82a3a21540c83deb58d83773709bbb3
xLsDg <xlsdg@qq.com>
2019-01-24 15:49:00 +0800

    chore(mod): upgrade packages

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,37 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withRouter from 'umi/withRouter';
 import { Layout } from 'antd';
 
 import styles from './index.less';
 
-function renderHeader(props) {
-  return <Layout.Header className={styles.header}>Header</Layout.Header>;
+class BasicLayout extends React.Component {
+  state = {};
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  static defaultProps = {};
+
+  componentDidUpdate(prevProps) {
+    const that = this;
+    const { location } = that.props;
+
+    if (location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  renderHeader = () => {
+    return <Layout.Header className={styles.header}>Header</Layout.Header>;
+  };
+
+  renderContent = () => {
+    const that = this;
+    const { children } = that.props;
+
+    return <Layout.Content className={styles.content}>{children}</Layout.Content>;
+  };
+
+  renderFooter = () => {
+    return <Layout.Footer className={styles.footer}>Footer</Layout.Footer>;
+  };
+
+  render() {
+    const that = this;
+
+    return (
+      <Layout className={styles.main}>
+        {that.renderHeader()}
+        {that.renderContent()}
+        {that.renderFooter()}
+      </Layout>
+    );
+  }
 }
 
-function renderContent(props) {
-  const { children } = props;
-
-  return <Layout.Content className={styles.content}>{children}</Layout.Content>;
-}
-
-function renderFooter(props) {
-  return <Layout.Footer className={styles.footer}>Footer</Layout.Footer>;
-}
-
-function BasicLayout(props) {
-  return (
-    <Layout className={styles.main}>
-      {renderHeader(props)}
-      {renderContent(props)}
-      {renderFooter(props)}
-    </Layout>
-  );
-}
-
-BasicLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-// BasicLayout.defaultProps = {};
-
-export default BasicLayout;
+export default withRouter(BasicLayout);
